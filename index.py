9be731526c4a77d1ec83e815e20aedc1fa5d4188
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-05 20:08:39 -0300

    Adicionando arquivo python base.

---
+++ index.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+
+    region = request.form['region']
+    species = request.form['species']
+    conservation_status = request.form['conservation_status']
+    water_temperature = request.form['water_temperature']
+    ph = request.form['ph']
+    pollution_leves = request.form['pollution_leves']
+    
+    api_url = {f'https://fiap-3sis-gs-20241.azurewebsites.net/OceanData?regiao={region}&especie={species}&statusConservacao={conservation_status}&temperaturaMin={water_temperature}&temperaturaMax={water_temperature}&phMin={ph}&phMax={ph}&nivelPoluicao={pollution_leves}'}
+    response = requests.get(api_url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+        return ""
+    else:
+        return "Erro ao acessar a API"
+
+if __name__ == '__main__':
+    app.run(debug=True)
